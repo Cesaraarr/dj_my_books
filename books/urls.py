6034e9fb851bf285e4f9fd815f2aca7f8e730e42
chrizmeza <angelariasmezacrack@gmail.com>
2026-04-22 00:46:59 -0700

    cambios para books

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from .views import home, detail
+from .views import home  
 
 urlpatterns = [
     path('', home, name='home'),
-    path('books/<int:id>/', detail, name='detail'),
+    
 ]
--- books/views.py
+++ books/views.py
@@ -1,21 +1,6 @@
+from django.shortcuts import render
+from .models import Book  
 
-from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, render
-
-from .models import Book, Category
-
-# Create your views here.
 def home(request):
-    books = Book.objects.filter(status=Book.ACTIVE).order_by('-created_at')
-    context = {
-        'books': books
-    }
-    return render(request, 'books/home.html', context)
-
-
-def detail(request, id):
-    books = get_object_or_404(Book, id=id, status=Book.ACTIVE)
-    context = {
-        'books': books,
-    }
-    return render(request, 'books/detail_book.html', context)
+    books = Book.objects.all()  
+    return render(request, 'books/home.html', {'books': books})  # ← Ruta correcta
