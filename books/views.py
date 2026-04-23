9b99a1dfce29b03b7ae02f4c2ff8e33bc4daa583
chrizmeza <angelariasmezacrack@gmail.com>
2026-04-23 12:37:38 -0700

    ultimos cambios

--- books/views.py
+++ books/views.py
@@ -3,4 +3,4 @@ from .models import Book
 
 def home(request):
     books = Book.objects.all()  
-    return render(request, 'books/home.html', {'books': books})  # ← Ruta correcta
+    return render(request, 'books/home.html', {'books': books})  
